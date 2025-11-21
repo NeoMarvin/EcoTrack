50df4545bf6d77cf267feb95be128e1c342cd4a1
NeoMarvin <marvinadegbite12@gmail.com>
2025-11-21 13:54:22 +0100

    footer correction

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function() {
     return (
         <footer className="bg-green-700 text-white text-center py-3">
-            <p>0 {new Date().getFullYear()} EcoTrack. All rights reserved.</p>
+            <p> {new Date().getFullYear()} EcoTrack. All rights reserved.</p>
         </footer>
     );
 }
